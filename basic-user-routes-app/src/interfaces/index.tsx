8b0a3eea4aade683fd91bd4dd2c1325169ee3147
TopengDev <topengdev@outlook.com>
2023-04-19 13:28:11 +0700

    created global custom interfaces

---
+++ basic-user-routes-app/src/interfaces/index.tsx
@@ -0,0 +1,51 @@
+import { ReactNode, Dispatch, SetStateAction } from "react";
+
+interface IDefaultProps {
+  children?: ReactNode;
+}
+
+export default IDefaultProps;
+
+export interface IUser {
+  gender: "l" | "p";
+  id: number;
+  file: string;
+  born_date: Date;
+  user_id: number;
+  address: string;
+  name: string;
+  photo: string;
+  created_at: Date;
+  user_name: string;
+}
+
+export interface IAddUserForm {
+  name: string;
+  address: string;
+  gender: "l" | "p";
+  born_date: Date;
+}
+
+export interface IEditUserForm extends IAddUserForm {
+  id: number;
+}
+
+export interface IMe {
+  id: number | null;
+  created_at: Date | null;
+  status: string | null;
+  name: string | null;
+  email: string | null;
+}
+
+export interface IFormState {
+  pOpt: 0 | 1;
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface IFormProps {
+  formState: IFormState;
+  setFormState: Dispatch<SetStateAction<IFormState>>;
+}
